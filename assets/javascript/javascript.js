0827f9330534cca6534a266a28fc55bfec84f46d
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-07-21 11:44:55 -0400

    starting js set up tags and simple styling

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,7 @@
+var introJumbo = document.getElementById("#introJumbo");
+var questionJumbo = document.getElementById("questionJumbo");
+var question = document.getElementById("#question");
+var answers = document.getElementById("#answers");
+var verdict = document.getElementById("#verdict");
+var timeDisplay = document.getElementById("timeDisplay");
+var startBtn = document.getElementById("#startBtn");
